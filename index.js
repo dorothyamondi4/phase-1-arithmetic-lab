4de3e4724ea9328bce44a56620adcf182fea02e4
dorothy <dorothy.amondi@student.moringaschool.com>
2022-08-21 11:12:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 =31;
+const num2 = 2;
+const multiply =num1 * num2
+
+let num3 = 9;
+let num4 = 5;
+const mod = num3 % num4;
+let max = (4,8,16,20);
+let random = 6
+Number.isInteger =true
+
